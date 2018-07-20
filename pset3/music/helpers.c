e24439f46edf8f8d733ad010729e6ac251cbd7ba
Jim Doyle <indellibleink@gmail.com>
2018-07-20 21:22:43 +0000

    Completed Pset3

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -2,43 +2,105 @@
 #include <math.h>
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "helpers.h"
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    // TODO
-    // 1/8 = 1 , 1/4 = 2, 3/8 = 3, 1/2 = 4 notes 2/8 with bar
-  int t = (int) fraction;
-
-   return t;
+    if(fraction[0] == '1')
+    {
+        if(fraction[2] == '8')
+        {
+            return 1;
+        }
+        else if(fraction[2] == '4')
+        {
+            return 2;
+        }
+        else if(fraction[2] == '2')
+        {
+            return 4;
+        }
+        else if(fraction[2] == '1')
+        {
+            return 8;
+        }
+    }
+    else if(fraction[0] == '3' && fraction[2] == '8')
+    {
+        return 3;
+    }
+    else
+    {
+        return 0;
+    }
+    return 0;
 
 }
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-  // TODO
-  // Math: Up: var=440*2^(n/12)  Down: var=440/2^(n/12)
-  // next octive: var= 880 * 2(n/12)
-  // ex.- 493.883301256 = 440*2^(2/12) rounding is needed
-  // tested in google calc to have any number in N and got a right answer.
-  int note=1
+    // TODO
 
-  if (note==1){
-      // up notes/octives
-    int u=12;
-    float uf=440*powf(2.0,(u/12.0));
-    int frequ=round(uf);
+    int n = note[strlen(note) - 1];
+    n -= 48;
+    double freq = 0.0;
 
+    switch(note[0])
+    {
+        case 'C' :
+            freq = 440.0 / (pow(2.0, (9.0 / 12.0)));
+            break;
+        case 'D' :
+            freq = 440.0 / (pow(2.0, (7.0 / 12.0)));
+            break;
+        case 'E' :
+            freq = 440.0 / (pow(2.0, (5.0 / 12.0)));
+            break;
+        case 'F' :
+            freq = 440.0 / (pow(2.0, (4.0 / 12.0)));
+            break;
+        case 'G' :
+            freq = 440.0 / (pow(2.0, (2.0 / 12.0)));
+            break;
+        case 'A' :
+            freq = 440.0;
+            break;
+        case 'B' :
+            freq = 440.0 * (pow(2.0, (2.0 / 12.0)));
+            break;
+        default :
+            return 0;
+    }
+    if(n > 4)
+    {
+        for(int i = 0; i < n - 4; i++)
+        {
+            freq *= 2.0;
+        }
+    }
+    else if(n < 4)
+    {
+        for(int i = 0; i < 4 - n; i++)
+        {
+            freq /= 2.0;
+        }
+    }
 
-} else{
-    // down notes/octives
-      int d=12;
-    float udf=440/powf(2.0,(d/12.0));
-    int frequ=round(df);
-}
-    return frequ;
+    if(note[1] == 'b')
+    {
+        freq /= (pow(2.0, (1.0 / 12.0)));
+    }
+    else if(note[1] == '#')
+    {
+        freq *= (pow(2.0, (1.0 / 12.0)));
+    }
+
+    int ret = round(freq);
+    return ret;
 
 }
 
@@ -46,10 +108,12 @@ int frequency(string note)
 bool is_rest(string s)
 {
     // TODO
-    string rest=get_string(s);
-    int r=(int) rest;
-    if (r ==10) {
+
+    if(strlen(s))
+    {
+    return 0;
+    }else {
         return 1;
     }
-}
 
+}
