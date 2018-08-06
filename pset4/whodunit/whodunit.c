309feba1ce8d397f431659873e5cb8031ea4639b
Jim Doyle <indellibleink@gmail.com>
2018-08-06 10:09:56 +0000

    updated file

--- pset4/whodunit/whodunit.c
+++ pset4/whodunit/whodunit.c
@@ -75,7 +75,8 @@ int main(int argc, char *argv[])
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-              if ( triple.rgbtRed==0xff && triple.rgbtBlue==0x00  && triple.rgbtGreen==0x00){
+              if ( triple.rgbtRed==0xff && triple.rgbtBlue==0x00  && triple.rgbtGreen==0x00)
+              {
               triple.rgbtRed=0xff;
               triple.rgbtBlue=0xff;
               triple.rgbtGreen=0xff;
